97be12a60fe0be44c8fead7da2c8f4d1f5aa7deb
VasthaviSreya-Thumu <22pa1a05g5@vishnu.edu.in>
2025-02-08 18:11:01 +0530

    First commit

---
+++ basics.java
@@ -0,0 +1,7 @@
+class basics {
+    public static void main (String Args[]) {
+        String s1 = " Hello World ";
+        char[] a = s1.toCharArray();
+        System.out.println(s1.toCharArray());
+    }
+}
